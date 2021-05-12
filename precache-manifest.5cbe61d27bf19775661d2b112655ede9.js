061c79c32f684ed7cb12eda28195092edfe6f984
Gopal Kaul <gopalkaul5@gmail.com>
2021-05-12 20:37:07 +0530

    Updates

---
+++ precache-manifest.5cbe61d27bf19775661d2b112655ede9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea0d837164662756ecfd",
+    "url": "/CoWIN-Tracker/static/css/main.671c6ea4.chunk.css"
+  },
+  {
+    "revision": "ea0d837164662756ecfd",
+    "url": "/CoWIN-Tracker/static/js/main.ec489f4a.chunk.js"
+  },
+  {
+    "revision": "9841be228ffe687be5f3",
+    "url": "/CoWIN-Tracker/static/js/runtime~main.d9cde4f1.js"
+  },
+  {
+    "revision": "f7695e9166977b894df9",
+    "url": "/CoWIN-Tracker/static/css/2.d486a0e4.chunk.css"
+  },
+  {
+    "revision": "f7695e9166977b894df9",
+    "url": "/CoWIN-Tracker/static/js/2.d29df4aa.chunk.js"
+  },
+  {
+    "revision": "fed0ddf00e08525a38fa",
+    "url": "/CoWIN-Tracker/static/js/3.d795ef33.chunk.js"
+  },
+  {
+    "revision": "b37e0c29c7d2872741b1654b9646f767",
+    "url": "/CoWIN-Tracker/index.html"
+  }
+];
