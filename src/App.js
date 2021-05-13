652fa1af28ffcf8ed7bce9947206313422bbfb0d
Gopal Kaul <gopalkaul5@gmail.com>
2021-05-13 17:32:31 +0530

    Fixed Submit bug, refactored the code to improve performance, added Vaccine Filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import "./App.css";
+import axios from "axios";
 import React, { useState } from "react";
-import Centres from "./FetchCentres";
-import { Container, Button ,Form,Label,Input } from "reactstrap";
+import Centre from "./components/centre";
+import {
+  Container,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Spinner,
+} from "reactstrap";
 import DatePicker from "react-date-picker";
 
 function App() {
   const [date, setDate] = useState(new Date());
   const [pincode, setPincode] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
+  const [centres, setCentres] = useState();
+  const [loading, setLoading] = useState(true);
+  const [empty, setEmpty] = useState(false);
+  const [vaccine, setVaccine] = useState("any");
+  const fetchCentres = () => {
+    const apiUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date.getDate()}-${
+      date.getMonth() + 1
+    }-${date.getFullYear()}`;
+    axios
+      .get(apiUrl)
+      .then((resp) => resp.data.centers)
+      .then((resp) => {
+        setCentres(resp);
+        setLoading(false);
+        console.log(resp);
+        if (resp.length === 0) setEmpty(true);
+      })
+      .catch((e) => console.log(`Error : ${e}`));
+  };
   return (
     <div className="App ">
       <Container>
@@ -25,31 +51,63 @@ function App() {
           <Label>Date : </Label>
           <DatePicker value={date} onChange={(d) => setDate(d)} />
           <br />
-          <Button color='success'
+          <FormGroup>
+            <Label for="vaccine">Vaccine</Label>
+            <Input
+              onChange={(e) => {
+                console.log(e.target.value);
+                setVaccine(e.target.value);
+              }}
+              type="select"
+              name="select"
+              id="vaccine"
+            >
+              <option>any</option>
+              <option>COVAXIN</option>
+              <option>COVISHIELD</option>
+            </Input>
+          </FormGroup>
+          <Button
+            color="primary"
             onClick={(e) => {
               e.preventDefault();
-              setSubmitted(true);
+              fetchCentres();
             }}
           >
             Submit
           </Button>
-          <Button color='danger'
-            onClick={(e) => {
-              e.preventDefault();
-              setSubmitted(false);
-            }}
-          >
-            Reset
-          </Button>
         </Form>
-        {submitted && (
-          <Centres
-            pincode={pincode.toString()}
-            date={`${date.getDate()}-${
-              date.getMonth() + 1
-            }-${date.getFullYear()}`}
-          />
-        )}
+        {empty && <h3>No Slots Available!</h3>}
+        {centres &&
+          (loading ? (
+            <Spinner color="primary" />
+          ) : (
+            <div>
+              {centres
+                .filter((x) => parseInt(x.sessions[0].available_capacity) > 0)
+                .filter((x) => {
+                  if(vaccine!=='any')
+                    return x.sessions[0].vaccine===vaccine;
+                  else
+                    return true;
+                })
+                .map((x, i) => {
+                  return (
+                    <div className="centres" key={i}>
+                      {" "}
+                      <Centre
+                        name={x.name}
+                        address={x.address}
+                        fee={x.fee_type}
+                        capacity={x.sessions[0].available_capacity}
+                        age={x.sessions[0].min_age_limit}
+                        vaccine={x.sessions[0].vaccine}
+                      />{" "}
+                    </div>
+                  );
+                })}
+            </div>
+          ))}
       </Container>
     </div>
   );
